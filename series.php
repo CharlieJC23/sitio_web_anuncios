de156b0e97930df947b868662d5aa6e122d53ef2
Yayo09 <63682340+Yayo09@users.noreply.github.com>
2020-06-10 03:25:17 -0500

    Commit Success!!!

--- series.php
+++ series.php
@@ -45,39 +45,69 @@
                     </ul>
                 </nav>
         </header>
+
+        <div id="menuopc">
+            <div style="width: 200px; height: 400;" class="container">
+                <h4>Genero:</h4>
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action">Accion</a>
+                    <a href="#" class="list-group-item list-group-item-action">Ciencia Ficcion</a>
+                    <a href="#" class="list-group-item list-group-item-action">Deportes</a>
+                    <a href="#" class="list-group-item list-group-item-action">Infantil</a>
+                    <a href="#" class="list-group-item list-group-item-action">Mecha</a>
+                    <a href="#" class="list-group-item list-group-item-action">Misterio</a>
+                    <a href="#" class="list-group-item list-group-item-action">Romance</a>
+                    <a href="#" class="list-group-item list-group-item-action">Shonen</a>
+                    <a href="#" class="list-group-item list-group-item-action">Terror</a>
+                </div>
+            </div>
+        </div>
+
+        <div id="artc">
+            <div class="container">
+                <div class="card" style="width:150px">
+                    <img class="card-img-top" src="image/SW/animal-crossing-nsw_1_1.jpg" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <h6 class="card-title" style="font-family: Century Gothic;">AC</h6>
+                        <span style="color: red">PRECIO: $1,499 </span>
+                        <center><a href="Articulo3.html" class="btn btn-danger btn-sm stretched-link ">COMPRAR</a></center>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <footer style="background-color: black;" class="page-footer font-small mdb-color  lighten-3 pt-4">
+        <div class="container text-center text-md-left">
+            <div class="row">
+                <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+                    <h5 style="color: white;" class="font-weight-bold text-uppercase mb-4">PublicaTec</h5>
+                    <p style="color: white;">Somos una pagina web que permite a cualquier
+                        usuario promocionar sus articulos de diversa indole.
+                        Ponemos a tu alcance un amplio catalogo donde podras encontrar lo que más te apasiona del mundo
+                        desde la comodidad de tu casa.</p>
+                </div>
+                <hr class="clearfix w-100 d-md-none">
+                <hr class="clearfix w-100 d-md-none">
+                <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+                    <h5 style="color: white;" class="font-weight-bold text-uppercase mb-4">ACERCA DE</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <p style="color: white;">Dirección: <br> Toluca, EDO MX 50260, MX</p>
+                        </li>
+                        <li>
+                            <p style="color: white;">Correo: <br> publicatec@ventaonline.com</p>
+                        </li>
+                        <li>
+                            <p style="color: white;">Tel: <br> + 52 219 123 10</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div style="color: white;" class="footer-copyright text-center py-3">© 2020<a href=""> PublicaTec.com</a>
+            </div>
+    </footer>
 </body>
-
-<footer style="background-color: black;" class="page-footer font-small mdb-color  lighten-3 pt-4">
-    <div class="container text-center text-md-left">
-        <div class="row">
-            <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
-                <h5 style="color: white;" class="font-weight-bold text-uppercase mb-4">PublicaTec</h5>
-                <p style="color: white;">Somos una pagina web que permite a cualquier
-                    usuario promocionar sus articulos de diversa indole.
-                    Ponemos a tu alcance un amplio catalogo donde podras encontrar lo que más te apasiona del mundo
-                    desde la comodidad de tu casa.</p>
-            </div>
-            <hr class="clearfix w-100 d-md-none">
-            <hr class="clearfix w-100 d-md-none">
-            <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
-                <h5 style="color: white;" class="font-weight-bold text-uppercase mb-4">ACERCA DE</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <p style="color: white;">Dirección: <br> Toluca, EDO MX 50260, MX</p>
-                    </li>
-                    <li>
-                        <p style="color: white;">Correo: <br> publicatec@ventaonline.com</p>
-                    </li>
-                    <li>
-                        <p style="color: white;">Tel: <br> + 52 219 123 10</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div style="color: white;" class="footer-copyright text-center py-3">© 2020<a href=""> PublicaTec.com</a>
-        </div>
-</footer>
 
 </html>

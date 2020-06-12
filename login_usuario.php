1f03bf89419b9a56ee845fbd715fe4ffe1531628
CharlieJC23 <66696360+CharlieJC23@users.noreply.github.com>
2020-06-12 14:45:43 -0500

    Update login_usuario.php

--- login_usuario.php
+++ login_usuario.php
@@ -13,34 +13,38 @@
 
     </head>
     <body>
-        <div class="modal-dialog text-center">
-            <div class="col-sm-8 main-section">
-                <div class="modal-content">
-                    <div class="col-12">
-                        <img src="resources/images/login.png">  
-                    </div>
-                    <form class="col-12" action="login.php" method="post" id="formlg">
-                        <div class="form-group">
-                            <label for="inputUsuario" class="col-sm-2 control-label">Usuario</label>
-                            <div class="col-12">
-                                <input type="text" class="form-control" name="usuariolg" id="inputEmail3" placeholder="usuario" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPassword" class="col-sm-2 control-label">Password</label>
-                            <div class="col-12">
-                                <input type="password" class="form-control" name="passlg" id="inputPassword3" placeholder="Password" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-success btonlg">Iniciar Sesion</button>
-                            </div>
-                        </div>
-                        </form>
-                </div> 
-            </div>
-        </div>
-        <p>prueba</p>
+        <!-- FORMULARIO-->
+	<div class="modal-dialog text-center">
+		<div class="col-sm-8 main-section">
+			<div class="modal-content">
+				<div class="col-12">
+                <img src="resources/images/login.png">
+				</div>
+				<form class="col-12" action="login.php" method="post">
+					<div class="form-group">
+						<label for="inputUsuario" class="col-sm-2 control-label">Usuario</label>
+						<div class="col-12">
+							<input type="text" class="form-control" name="usuario" id="inputEmail3" placeholder="usuario" required>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="inputPassword" class="col-sm-2 control-label">Password</label>
+						<div class="col-12">
+							<input type="password" class="form-control" name="password" id="inputPassword3" placeholder="Password" required>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-12">
+							<button type="submit" class="btn btn-default">Iniciar Sesion</button>
+						</div>
+						<div>
+							<a href="registro.php">Registrarse</a>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+    <!-- TERMINA FORMULARIO-->
     </body>
 </html>

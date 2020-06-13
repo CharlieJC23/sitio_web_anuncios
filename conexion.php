e9e76a3102c7440fc86318edf30ab9fc824a9453
CharlieJC23 <66696360+CharlieJC23@users.noreply.github.com>
2020-06-13 00:30:00 -0500

    Agrego alerta de jquery

--- conexion.php
+++ conexion.php
@@ -1,15 +1,62 @@
-    <?php
-        session_start();
-        // Datos para conectar a la base de datos.
-        $nombreServidor = "localhost";
-        $nombreUsuario = "root";
-        $passwordBaseDeDatos = "";
-        $nombreBaseDeDatos = "videojuegos";
+<html>
 
-        // Crear conexión con la base de datos.
-        $conn = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
-       // Validar la conexión de base de datos.
-  if ($conn ->connect_error) {
-    die("Connection failed: " . $conn ->connect_error);
+<head>
+  <title>Login usuarios con PHP y JQuery</title>
+  <meta charset='utf-8'>
+  <meta htPage Titletp-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+  <!-- BOOTSTRAP Y JQUERY AGREGADOS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<!-- Validacion para el formulario con jquery-->
+<script>
+  function ValidaUsuario(){
+    alert("Usuario incorrecto").slideUp(500);
   }
-    ?>
+  function ValidaContrasena(){
+    alert("Contraseña incorrecta").slideUp(500);
+  }
+  
+</script>
+
+<body>
+  <?php
+  session_start();
+  // Crear conexión con la base de datos.
+  $con = new mysqli("localhost", "root", "", "videojuegos");
+  if ($con->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+    exit();
+  }
+  @mysqli_query($con, "SET NAMES 'utf8'");
+  ?>
+  <?php
+  //valirdar desde jquery
+  $usuario = $_POST['inputUsuario'];
+  $contrasena = $_POST['inputPassword'];
+
+  // Consulta para comprobar usuario y contraseña.
+  $usuario_bd = sprintf("SELECT usuario FROM usuarios WHERE usuario=$usuario");
+  $comprueba_usuario = $conn->query($usuario_bd);
+
+  $contrasena_bd = sprintf("SELECT Password FROM usuarios WHERE Password=$contrasena");
+  $comprueba_contrasena = $conn->query($contrasena_bd);
+
+  //mandar mensaje con jquery
+  if ($comprueba_usuario==false) {
+    echo '<script type="text/javascript">',
+    'validaUsuario();',
+    '</script>';
+  }else if($comprueba_contrasena){
+    echo '<script type="text/javascript">',
+    'validaContrasena();',
+    '</script>';
+  }else{
+    echo'Error inesperado';
+  }
+  ?>
+</body>
+
+</html>
--- login_usuario.php
+++ login_usuario.php
@@ -1,50 +1,9 @@
 <html>
-
 <head>
 	<title>Login usuarios con PHP y JQuery</title>
 	<meta charset='utf-8'>
 	<meta htPage Titletp-equiv='X-UA-Compatible' content='IE=edge'>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
-	<!-- BOOTSTRAP Y JQUERY AGREGADOS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<!-- Validacion para el formulario con jquery-->
-	<!-- Validacion para el formulario con jquery-->
-	<script>
-		function Validar() {
-			var usuario = $('#inputUsuario');
-			var password = $('#inputPassword');
-			var bandera = false;
-			require("conexion.php");
-
-			<?php
-			session_start();
-			// Consulta para comprobar usuario y contraseña.
-			$usuario_bd = sprintf("SELECT usuario FROM usuarios WHERE usuario=$usuario");
-			$comprueba_usuario = $conn->query($usuario_bd);
-
-			$contrasena_bd = sprintf("SELECT Password FROM usuarios WHERE Password=$contrasena");
-			$comprueba_contrasena = $conn->query($contrasena_bd);
-			?>
-			
-			// Verificando si el usuario existe en la base de datos.
-			if ($comprueba_usuario && $comprueba_contrasena) {
-				bandera = true;
-			} else if ($comprueba_usuario == false) {
-				alert("Usuario incorrecto").slideUp(500);
-			} else if ($comprueba_contrasena == false) {
-				alert("Contraseña incorrecta").slideUp(500);
-			}
-
-			//Direccionamiento a la pagina principal
-			if (bandera) {
-				// Redirecciono al usuario a la página principal del sitio.
-				$(location).attr('href', menu_usuario.php);
-			} else {
-				alert("Conexion fallida").slideUp(500);
-			}
-		} //Termina Validacion
-	</script>
 	<!--ESTILO DEL FORMULARIO-->
 	<style type="text/css">
 		body {
@@ -68,7 +27,7 @@
 				<div class="col-12">
 					<img src="resources/images/login.png">
 				</div>
-				<form class="col-12" action="" method="post" onsubmit="javascript: return Validar(this);">
+				<form class="col-12" action="conexion.php" method="post">
 					<div class="form-group">
 						<label for="inputUsuario" class="col-sm-2 control-label">Usuario</label>
 						<div class="col-12">

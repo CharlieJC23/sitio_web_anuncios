71b8657b8a326d2d716a87aeb8bbb047a31b982d
CharlieJC23 <66696360+CharlieJC23@users.noreply.github.com>
2020-06-14 18:18:38 -0500

    que hice

--- ajaxRegister.php
+++ ajaxRegister.php
@@ -12,10 +12,17 @@ session_start();
 $username=mysqli_real_escape_string($db,$_POST['username']); 
 $password=mysqli_real_escape_string($db,$_POST['password']);
 
+// Consulta segura para evitar que haya dos usuarios con el mismo nombre
+$comprueba=mysqli_query($db,"SELECT usuario FROM usuarios WHERE usuario='$username';");
+
+// Verificando si el usuario existe en la base de datos.
+if($comprueba){
+    echo'Usuario no displonible';   
+}else{
 $result=mysqli_query($db,"INSERT INTO usuarios(id_usuario,usuario,password,tipo_usuario) VALUES(null,'$username','$password','admin');");
 $count=mysqli_num_rows($result);
 $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-
+    }
 // If result matched $username and $password, table row  must be 1 row
 if($count==1)
 {
